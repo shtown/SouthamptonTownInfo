49aea7a818972692697c72d729d1e0383609313c
John Daly <jdaly@southamptontownny.gov>
2017-06-29 14:13:31 -0400

    Initial Commit

---
+++ SouthamptonTownInfo/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  SouthamptonTownInfo
+//
+//  Created by Southampton Dev on 6/29/17.
+//  Copyright © 2017 TOS. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
