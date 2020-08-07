7e309dc1b75c318a13f38e6e9dc76d3b1ea3a743
John Daly <jdaly@southamptontownny.gov>
2020-08-07 14:44:50 -0400

    Updated URLs for MyTownInfo

--- SouthamptonTownInfo/DataViewController.swift
+++ SouthamptonTownInfo/DataViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import SafariServices
 
 class DataViewController: UIViewController {
 
-    @IBOutlet weak var dataLabel: UILabel!
+   // @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
-
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -26,9 +28,61 @@ class DataViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
+        //self.dataLabel!.text = dataObject 
     }
 
+    
+    @IBAction func sosClicked(_ sender: Any) {
+        
+        let url = URL(string: "https://gis.southamptontownny.gov/sos")
+        let safariVC: SFSafariViewController = SFSafariViewController(url: url!)
+        
+        self.present(safariVC, animated: true, completion: nil)    }
+    
+   @IBAction func infoCenterClicked(_ sender: Any){
+        
+        
+        let url = URL(string: "https://gis.southamptontownny.gov/infocenter")
+        let safariVC: SFSafariViewController = SFSafariViewController(url: url!)
+        
+        self.present(safariVC, animated: true, completion: nil)
+    
+    }
+ 
+    
+    @IBAction func ePortalClicked(_ sender: Any) {
+    
+        let url = URL(string: "https:/eportal.southamptontownny.gov")
+        let safariVC: SFSafariViewController = SFSafariViewController(url: url!)
+        
+        self.present(safariVC, animated: true, completion: nil)
+    }
 
+    @IBAction func salesMapClicked(_ sender: Any) {
+     
+        let url = URL(string: "https://gis.southamptontownnygov/sales")
+        let safariVC: SFSafariViewController = SFSafariViewController(url: url!)
+        
+        self.present(safariVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func historicMapClicked(_ sender: Any) {
+    
+        let url = URL(string: "https://gis.southamptontownny.gov/historic")
+        let safariVC: SFSafariViewController = SFSafariViewController(url: url!)
+        
+        self.present(safariVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func evacZonesClicked(_ sender: Any) {
+        
+        let url = URL(string: "https://gis.southamptontownny.gov/evaclocator")
+        let safariVC: SFSafariViewController = SFSafariViewController(url: url!)
+        
+        self.present(safariVC, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
